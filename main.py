b7480d9381779ee21916b0debe6d766ea6105801
sbagzz <gnoronha409@gmail.com>
2026-03-15 23:17:12 -0300

    Fix: Adds validation to the program's loop.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import secrets
 import string
-
+import os 
 
 def gerar_hexadecimal(tamanho: int) -> str:
     return secrets.token_hex(tamanho)
@@ -14,6 +14,9 @@ def gerar_caracteres(tamanho: int) -> str:
     alfabeto = string.ascii_letters + string.digits + string.punctuation
     return "".join(secrets.choice(alfabeto) for _ in range(tamanho))
 
+def limpar_tela():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
 
 def main():
     while True:
@@ -21,42 +24,54 @@ def main():
             "Tipo de senha (hexadecimal, bytes, caracteres): "
         ).lower().strip()
 
+        limpar_tela()
+
         if opcao not in ("hexadecimal", "bytes", "caracteres"):
             print("Opção inválida! digite novamente")
             continue
+   
+        limpar_tela()
+
 
         try:
+            
             tamanho = int(input("Quantidade de caracteres: "))
             if tamanho <= 0:
                 
                 raise ValueError
         except ValueError:
+
             print("Digite um número válido maior que zero.")
-            continue
-            
+            continue  
+
         if opcao == "hexadecimal":
             print("Senha:", gerar_hexadecimal(tamanho))
+            
 
         elif opcao == "bytes":
             print("Senha:", gerar_bytes(tamanho))
+        
 
         elif opcao == "caracteres":
             print("Senha:", gerar_caracteres(tamanho))
-
+            
         else:
             print("Opção inválida.")
             continue
 
-        print("-" * 40)
+        while True:
+            resposta = input("Gerar outra senha? (s/n): ").strip().lower()    
 
-        resposta = input("Gerar outra senha? (s/n): ").strip().lower()
-        if resposta in ("sim", "s", "yes", "y"):
-            continue
+            if resposta in ("sim", "s", "yes", "y"):
+                limpar_tela()
+                break
 
-        else:
-
-            break
-
+            elif resposta in ("não", "n", "no", "n"):
+                print("Encerrando o programa.")
+                exit()
+                
+            else:
+                print("Resposta inválida! Digite 's' para sim ou 'n' para não.")
 
 if __name__ == "__main__":
     main()
